29144c387aeff9ddb3ef40bfe72f4525a2b9f30d
dakotaberg123 <dakotaberg123@hotmail.com>
2015-06-04 01:23:02 -0700

    Donuts should be working in directive.

--- js/directives.js
+++ js/directives.js
@@ -215,19 +215,42 @@ angular.module('DeViine.directives', [])
       restrict: 'A',
       link: function(scope, element, attrs) {
 
-        //Will watch for changes on the attribute
-        // attrs.$observe('data',function(){
-        //   console.log('Strain profile received from database. Executing drawDonuts();');
-        //   drawDonuts();
-        // })
+        // Will watch for changes on the attribute
+        attrs.$observe('data',function(){
+          console.log('Strain profile received from database. Executing drawDonuts();');
+          setTimeout(drawDonuts, 100);
+          drawDonuts();
+        })
         
-        drawDonuts();
+        // drawDonuts();
 
         function drawDonuts(){
 
-          drawDonut("#graph1");
-          drawDonut("#graph2");
-          drawDonut("#graph3");
+          var graph1 = $('#graph1').children();
+          var graph2 = $('#graph2').children();
+          var graph3 = $('#graph3').children();
+          var graphs = graph1, graph2, graph3;
+          // @todo = If donuts already exist, replace them.
+          if (! graphs) {
+            console.log('var of graphs does not exist. drawing donuts.');
+            drawDonut("#graph1");
+            drawDonut("#graph2");
+            drawDonut("#graph3");
+          } else {
+            console.log('var of graphs does exist. replacing donuts.');
+            graph1.remove();
+            graph2.remove();
+            graph3.remove();
+            drawDonut("#graph1");
+            drawDonut("#graph2");
+            drawDonut("#graph3");
+          }
+
+          console.log('Drawing donuts.');
+
+          // drawDonut("#graph1");
+          // drawDonut("#graph2");
+          // drawDonut("#graph3");
           /* 
             TODO:
             add an addtional data attr called "data-backup" which will hold the src
@@ -278,80 +301,6 @@ angular.module('DeViine.directives', [])
                   };
                 });
               }
-
-
-            // if (! amounts) {
-            //   console.log('No data yet. Trying again.');
-            //   var amounts = node.getAttribute("data-amounts").split(",");
-            //   var radius = Math.min(width, height) / 2;
-            //   var pie = d3.layout.pie().sort(null);
-              
-            //   var svg = d3.select(elem).append("svg")
-            //       .attr("width", width)
-            //       .attr("height", height)
-            //       .append("g")
-            //       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-              
-            //   var arc = d3.svg.arc()
-            //       .innerRadius(radius - thickness)
-            //       .outerRadius(radius);
-              
-            //   svg.selectAll("path")
-            //     .data(pie(amounts))
-            //     .enter()
-            //     .append("path")
-            //     .style("fill", function(d, i) { return fills[i]; })
-            //     .attr("d", arc)
-            //     .transition()
-            //     .delay(delay)
-            //     .duration(duration)
-            //     .call(arcTween);
-              
-            //   function arcTween(transition) {
-            //     transition.attrTween("d", function(d) {
-            //       var interpolate = d3.interpolate(d.startAngle, d.endAngle);
-            //       return function(t) {
-            //         d.endAngle = interpolate(t);
-            //         return arc(d);
-            //       };
-            //     });
-            //   }
-            // } else {
-            //   var radius = Math.min(width, height) / 2;
-            //   var pie = d3.layout.pie().sort(null);
-              
-            //   var svg = d3.select(elem).append("svg")
-            //       .attr("width", width)
-            //       .attr("height", height)
-            //       .append("g")
-            //       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
-              
-            //   var arc = d3.svg.arc()
-            //       .innerRadius(radius - thickness)
-            //       .outerRadius(radius);
-              
-            //   svg.selectAll("path")
-            //     .data(pie(amounts))
-            //     .enter()
-            //     .append("path")
-            //     .style("fill", function(d, i) { return fills[i]; })
-            //     .attr("d", arc)
-            //     .transition()
-            //     .delay(delay)
-            //     .duration(duration)
-            //     .call(arcTween);
-              
-            //   function arcTween(transition) {
-            //     transition.attrTween("d", function(d) {
-            //       var interpolate = d3.interpolate(d.startAngle, d.endAngle);
-            //       return function(t) {
-            //         d.endAngle = interpolate(t);
-            //         return arc(d);
-            //       };
-            //     });
-            //   }
-            // }
-
             
           }
         }
